3c04196827bedc3d55a96213a43c00f9ba98c975
Blaine Price <1wbprice@gmail.com>
2016-07-06 14:04:40 -0400

    Remove commented code related to Classes controller.

--- api/controllers/ClassesController.js
+++ api/controllers/ClassesController.js
@@ -6,18 +6,6 @@ const Controller = require('trails-controller')
  * @module ClassesController
  * @description Generated Trails.js Controller.
  */
-module.exports = class ClassesController extends Controller{
-
-  // list (request, response) {
-  //
-  // }
-  //
-  // create (request, response) {
-  //
-  // }
-  //
-  // destroy (request, response) {
-  //
-  // }
+module.exports = class ClassesController extends Controller {
 
 }
