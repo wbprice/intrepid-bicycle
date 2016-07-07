8a0e93a2d57c6756a35c38d23919f542241622a3
Blaine Price <1wbprice@gmail.com>
2016-07-07 09:09:14 -0400

    Fill out auth controller.

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -10,6 +10,17 @@ module.exports = class AuthController extends Controller {
 
   verify (request, reply) {
 
+    const emailAddress = request.payload.emailAddress
+    const plaintextPassword = request.payload.plaintextPassword
+
+    this.app.services.AuthService.verify(emailAddress, plaintextPassword)
+    .then(response => {
+      reply(response)
+    })
+    .catch(error => {
+      reply(error)
+    })
+
   }
 
 }
